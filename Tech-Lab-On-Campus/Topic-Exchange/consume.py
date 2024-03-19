2cd4123600359f82b799b771c1d5950fc8560993
the0123 <amdetheodros@gmail.com>
2024-03-18 22:34:31 -0400

    Updated Keys

--- Tech-Lab-On-Campus/Topic-Exchange/consume.py
+++ Tech-Lab-On-Campus/Topic-Exchange/consume.py
@@ -23,7 +23,7 @@ def main(sector: str, queueName: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
-    bindingKey = sector
+    bindingKey = f"#.{sector}.#"
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
     
--- Tech-Lab-On-Campus/Topic-Exchange/publish.py
+++ Tech-Lab-On-Campus/Topic-Exchange/publish.py
@@ -27,7 +27,7 @@ def main(ticker: str, price: float, sector: str) -> None:
    
     
 
-    routingKey = sector
+    routingKey = f"{sector}.{ticker}"
     producer = mqProducer(routing_key=routingKey,exchange_name="Tech Lab Topic Exchange")
 
     # Implement Logic To Create a message variable from the variable EG. "TSLA price is now $500" - Step 3
