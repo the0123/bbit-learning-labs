856b07bb467875c28bebcc550fa7ac0da27fd8cd
YonasWorku <Yo.worku@ufl.edu>
2024-03-18 22:00:15 -0400

    topic exchange from producer

--- Tech-Lab-On-Campus/Topic-Exchange/publish.py
+++ Tech-Lab-On-Campus/Topic-Exchange/publish.py
@@ -24,17 +24,19 @@ def main(ticker: str, price: float, sector: str) -> None:
     #
     #                       WRITE CODE HERE!!!
     #
+   
+    
 
-
+    routingKey = sector;
     producer = mqProducer(routing_key=routingKey,exchange_name="Tech Lab Topic Exchange")
-
 
     # Implement Logic To Create a message variable from the variable EG. "TSLA price is now $500" - Step 3
     #
     #                       WRITE CODE HERE!!!
     #
+   
     
-    
+    message = "Bob is 111"
     producer.publishOrder(message)
 
 if __name__ == "__main__":
@@ -43,5 +45,11 @@ if __name__ == "__main__":
     #
     #                       WRITE CODE HERE!!!
     #
+    
+    ticker = sys.argv[1]
+    price = float(sys.argv[2])
+    sector = sys.argv[3]
+
+       
 
     sys.exit(main(ticker,price,sector))
